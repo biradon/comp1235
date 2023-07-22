feea8552c84c023bcbb561ac74c8c04588903f81
Minh Truong <61044654+biradon@users.noreply.github.com>
2023-07-22 08:34:37 -0400

    update final

--- assignment3/101411424-problems.js
+++ assignment3/101411424-problems.js
@@ -1,166 +1,76 @@
 "use strict";
 
 // Function 1 
-
-let radius = 9; 
-let finalResult1 = circle(radius); 
-console.log(finalResult1); 
-
-function circle(radius) 
-{ 
-    let areaResult = area(radius); 
-    let perimeterReuslt = perimeter(radius); 
-    return {areaResult, perimeterReuslt} 
-}; 
-
-function area(radius) 
-{ 
-    // Formula to calculate area radius^2 * pi
-    let areaResult = Math.pow(radius,2) * Math.PI; 
-    return areaResult.toFixed(2); 
-} 
-
-function perimeter() 
-{ 
-    // Formula to calculate perimeter 2 * pi * radius
-    let perimeterResult = 2 * Math.PI * radius; 
-    return perimeterResult.toFixed(2); 
-} 
+function Circle(radius) {
+    this.area = function () {
+        return parseFloat((Math.pow(radius, 2) * Math.PI).toFixed(2));
+    };
+    this.perimeter = function () {
+        return parseFloat((2 * Math.PI * radius).toFixed(2));
+    };
+};
 
  
 
 // Function 2 
-let days = ([10,10,10,0,8,0,0]); 
+const calculateSalary = (days) => {
+    const result = days.reduce((totalSalary, hours, dayIndex) => {
+        //Normal working day
+        const regularHours = Math.min(hours, 8) * 10;
 
-// Create the arrays to assign new value to calculate
-let days1 = [];
+        // When working overtime 
+        const extraHours = hours > 8 ? (hours - 8) * 15 : 0;
 
-// Calculate the salary
-let calculateSalary = (days) =>
-{ 
-    let salary = 0;
-    for (let i = 0; i < days.length; i++) 
-    { 
-        // Check if the day is the weekend or not
-        if (days[i] == days[days.length - 1] || days[i] ==  days[days.length - 2])
+        // When working during weekend
+        const weekendHour = dayIndex > 4 ? (regularHours + extraHours) * 2 : 0;
+
+        // If it's weekend, only sum weekendhour, else sum everything
+        if (dayIndex > 4)
         {
-            // If the weekend double salary
-            salary = days[i] * 2;
-            // Add value inside created array
-            days1[i] = salary;
-        } 
-        // Check if hours is normal
-        else if (days[i] <= 8) 
-        { 
-            salary = days[i] * 10; 
-            // Add value inside created array
-            days1[i] = salary;
-        } 
-        // Check if hours is overtime
-        else if (days[i] > 8) 
-        { 
-            // 8 hours still calculate in mornal, but the overtime hours will be multiple with 15
-            salary = 8 * 10 + (days[i] - 8) * 15; 
-            // Add value inside created array
-            days1[i] = salary;
-        } 
-    } 
-
-    // Using reduce function to sum all of the value in new array
-    const finalResult2 = days1.reduce((monday, sunday) => {
-        return monday + sunday
-    })
-    return finalResult2
+            return totalSalary + weekendHour
+        } else
+        {
+            return totalSalary + regularHours + extraHours + weekendHour;
+        }
+    }, 0); 
+    return result;
 } 
 
 
-let finalResult2 = calculateSalary(days)
-console.log(finalResult2);
-
-
-
 // Function 3
-let example1 = ([1, 2, 3, 4, 5]);
-let example2 = ([-3, 0, 8, -6]);
-let example3 = ([15, 16, -100, 50]);
-
-// Create temporary array to store the value
-let newArray = [];
-
-
-let indexMultipler = (index) => {
-    for (let i = 0; i < index.length; i++)
-    {
-        // Assign for the new array to multiple value with position
-        newArray[i] = index[i] * i;
-    }
-    // Sum all of the value in array
-    const finalResult3 = newArray.reduce((begin, end) => {
-        return begin + end
-    })
-    return finalResult3;
+const indexMultiplier = (index) => {
+    const calculate = index.reduce((accumulator, currentValue, index) => {
+        return accumulator + (currentValue * index);
+      }, 0);
+      return calculate;
 }
-
-let finalResult3 = indexMultipler(example1)
-console.log(finalResult3);
 
 // Function 4
-let JSON1 = [{
-    id: 1, 
-    u: "batman"
-}];
-let JSON2 = [{
-    id: 11, 
-    u: "spidey"
-}];
-
-let JSON3 = [
-    {id: 1, u: "batman"}, 
-    {id: 11, u: "spidey"}
-];
-
-let pattern = /^[m-z]/i;
-
-
-let filteredJSON = (JSON0) => {
-    if (JSON0.id > 10 && pattern.test(JSON0.u))
-    {
-        return true;
-    }
-    return false;
+const filteredJSON = (JSON) => {
+    // Regex to find the chacracter in range [M-Z] and ignore case-sensitive
+    let pattern = /^[m-z]/i;
+    
+    // Filter the result, greater than 10 and with the regex above
+    let result = JSON.filter(JSON => JSON.id > 10 && pattern.test(JSON.u))
+    return result;
 }
 
-const finalResult4 = JSON3.filter(filteredJSON);
-console.log(finalResult4);
 
 
 // Function 5
-let sample1 = ([2,3,4,5]);
-let nsample1 = 2;
-
-let sample2 = ([2,3,4,5,6]);
-let nsample2 = 2;
-
-let sample3 = ([2,3,4,5,6,7]);
-let nsample3 = 3;
-
-let sample4 = ([2,3,4,5]);
-let nsample4 = 1;
-
-let sample5 = ([2,3,4,5,6,7]);
-let nsample5 = 7;
-
-let devidedNumber = 0;
-
-let finalResult5 = [];
-
 const breakAway = function(sample, nsample) {
+    // Create the number to devided
+    let devidedNumber = 0;
+
+    // Create the array to store the result
+    let finalResult5 = [];
+
+    // Round the number after devided to become a real number
     devidedNumber = Math.ceil(sample.length / nsample);
     for (let i = 0; i < devidedNumber; i++)
     {
+        // Separate the array
         finalResult5[i] = sample.splice(0, nsample);
     }
     return finalResult5;
 }
-
-console.log(breakAway(sample4, nsample4));
--- assignment3/tests-assign3.js
+++ assignment3/tests-assign3.js
@@ -4,43 +4,54 @@ const chai = window.chai;
 const expect = chai.expect;
 
 /***************************************************************************************
-describe('yourFunctionName', () =>  {
+ describe('yourFunctionName', () =>  {
     it('please enter your function description here',  () => {
-        expect(yourFunctionName(parameters)).to.deep.equal(expected_result)
+        expect(yourFunctionName(parameters)).to.deep.equal(expected_result);
     })
 })
  ***************************************************************************************/
 
-describe('circle', () =>  {
-    it('Calculate the area and perimeter by given circle radius',  () => {
-        expect(circle(3)).to.deep.equal(28.27, 18.85)
+describe('Circle(radius)', () =>  {
+    it('this Circle constructor with area and perimeter',  () => {
+        expect(new Circle(3).area()).to.deep.equal(28.27);
+        expect(new Circle(3).perimeter()).to.deep.equal(18.85);
+        expect(new Circle(9).area()).to.deep.equal(254.47);
+        expect(new Circle(9).perimeter()).to.deep.equal(56.55);
     })
 })
 
-describe('calculateSalary', () =>  {
-    it('Calculate the area and perimeter by given circle radius',  () => {
-        expect(calculateSalary([8,8,8,8,8,0,0])).to.deep.equal(400)
+describe('calculateSalary(array)', () =>  {
+    it('this function calculate and returns calculated total gross salary',  () => {
+        expect(calculateSalary([8,8,8,8,8,0,0])).to.deep.equal(400);
+        expect(calculateSalary([10,10,10,0,8,0,0])).to.deep.equal(410);
+        expect(calculateSalary([0,0,0,0,0,12,0])).to.deep.equal(280);
     })
 })
 
-describe('indexMultipler', () =>  {
-    it('Multiple the array value with array position',  () => {
-        expect(indexMultipler([1, 2, 3, 4, 5])).to.deep.equal(40)
+describe('indexMultiplier(array)', () =>  {
+    it('this function calculates the sum of all items in the array, each multiplied by its index',  () => {
+        expect(indexMultiplier([1,2,3,4,5])).to.deep.equal(40);
+        expect(indexMultiplier([-3,0,8,-6])).to.deep.equal(-2);
+        expect(indexMultiplier([15,16,-100,50])).to.deep.equal(-34);
     })
 })
 
-describe('filteredJSON', () =>  {
-    it('Filter the JSON object with specific condition',  () => {
-        expect(filteredJSON(([{id: 1, u: "batman"}])).to.deep.equal([]))
-        expect(filteredJSON(([{id: 11, u: "spidey"}])).to.deep.equal([{id: 11, u: "spidey"}]))
-
+describe('filteredJSON(array)', () =>  {
+    it('return filters an array of JSON elements (id greater than 10, username within [M-N])',  () => {
+        expect(filteredJSON([{id: 1, u: "batman"}, {id: 11, u: "spidey"}])).to.deep.equal([{id: 11, u: "spidey"}]);
+        expect(filteredJSON([{id:1, u:"batman"}])).to.deep.equal([]);
+        expect(filteredJSON([{id: 11, u: "batman"}])).to.deep.equal([]);
+        expect(filteredJSON([{id: 11, u: "spidey"}])).to.deep.equal([{id: 11, u: "spidey"}]);
     })
 })
 
 describe('breakAway', () =>  {
-    it('Break the array and combine it with the given number',  () => {
-        expect(breakAway(([2,3,4,5], 2)).to.deep.equal([[2,3],[4,5]]))
-        expect(breakAway(([2,3,4,5,6,7], 3)).to.deep.equal([2,3],[4,5],[6] ))
-
+    it('This function divides an array (first argument) into chuck of size n (second parameter)',  () => {
+        expect(breakAway([2, 3], 2)).to.deep.equal([ [2, 3]])
+        expect(breakAway([2,3,4,5], 2)).to.deep.equal([ [2,3], [4,5] ])
+        expect(breakAway([2,3,4,5,6], 2)).to.deep.equal([ [2,3], [4,5], [6] ])
+        expect(breakAway([2,3,4,5,6,7], 3)).to.deep.equal([ [2,3,4], [5,6,7] ])
+        expect(breakAway([2,3,4,5], 1)).to.deep.equal([ [2],[3],[4],[5] ])
+        expect(breakAway([2,3,4,5,6,7], 7)).to.deep.equal([ [2,3,4,5,6,7] ])
     })
 })
